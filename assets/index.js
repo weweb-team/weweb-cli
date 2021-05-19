5c20f9d3108e6551a5a2b427ae6df5cb0bbbd4de
Aurélie VIOLETTE <aurelie.violette@purple-fox.fr>
2021-05-19 12:02:33 +0200

    Handle relative path

--- assets/index.js
+++ assets/index.js
@@ -1,31 +1,29 @@
-import component from '../../test-react/./src/wwElement.jsx'
+
+        import component from '../../plugin-airtable/src/index.js'
+        import configuration from '../../plugin-airtable/ww-config.json'
 
         const name = "__NAME__";
         const version = '__VERSION__';
 
-        const addComponent = function () {
+        const config = { ...configuration };
+        if (name.indexOf('__') !== 0){
+            config.name = name;
+        }
+        config.wwDev = false,
+        config.port = undefined;
+
+        function addComponent() {
             if (window.vm) {
-
-                const config = {"componentPath":"./src/wwElement.jsx","isReact":true,"editor":{"label":{"en":"My Element"},"styleOptions":{"textColor":{"label":{"en":"Text color"},"type":"Color"}}}};
-                if(name.indexOf('__') !== 0){
-                    config.name = name;
-                }
-
-                config.wwDev = true,
-                config.port = 4040;
-
                 wwLib.wwComponents.register(config);
-                
                 window.vm.addComponent({
-                    name: name,
-                    version: version,
+                    name,
+                    version,
                     content: component,
-                    type: 'null',
-                    wwDev: true,
-                    port: 4040,
+                    type: 'plugin',
+                    wwDev: false,
+                    port: undefined,
                     config,
                 });
-            
                 return true;
             }
             return false;
--- bin/core/prebuild.js
+++ bin/core/prebuild.js
@@ -1,57 +1,57 @@
 #! /usr/bin/env node
 
 const fs = require("fs");
+const path = require("path");
+
+const PACKAGE_DIRECTORY = process.cwd();
+const ASSETS_DIRECTORY = path.resolve(__dirname, '../../assets');
+const RELATIVE_PATH = path.relative(ASSETS_DIRECTORY, PACKAGE_DIRECTORY);
+
+const CONFIG_PATH = path.join(PACKAGE_DIRECTORY, 'ww-config');
+const TMP_INDEX_PATH = path.join(ASSETS_DIRECTORY, 'index.js');
+
+console.log({PACKAGE_DIRECTORY, ASSETS_DIRECTORY, CONFIG_PATH, TMP_INDEX_PATH, RELATIVE_PATH});
 
 exports.prebuild = (options = {}) => {
     const wwDev = !!options.port;
     const type = options.type || null;
-    /*=============================================m_ÔÔ_m=============================================\
-        GET AND READ WW-CONFIG
-    \================================================================================================*/
-    let config;
-    let fileExtension;
-    // Get ww-config.json
-    if (fs.existsSync("./ww-config.json")) {
-        fileExtension = 'json';
-        config = fs.readFileSync("./ww-config.json");
-        try {
-            config = JSON.parse(config);
-        } catch (error) {
-            console.log('\x1b[41m Error : unable to parse "./ww-config.json". \x1b[0m');
-            return false;
-        }
 
-    } else if (fs.existsSync("./ww-config.js")) {
+    let fileExtension
+    if (fs.existsSync(`${CONFIG_PATH}.js`)) {
         fileExtension = 'js'
-        config = require('./ww-config.js')
-
+    } else if (fs.existsSync(`${CONFIG_PATH}.json`)) {
+        fileExtension = 'json'
     } else {
         console.log('\x1b[41mError : "./ww-config.js(on)" was not found.\x1b[0m');
         console.log('\x1b[44mTo create "./ww-config.js(on)" use "weweb create-config".\x1b[0m');
         return false;
     }
 
-    //Read ww-config.json
+    const config = require(CONFIG_PATH);
+
     let componentPath = config.componentPath || "./src/wwComponent.vue";
     console.log(`\x1b[44m Component Path : "${componentPath}" \x1b[0m`);
 
     if (!fs.existsSync(componentPath)) {
-        console.log(`\x1b[41m "${componentPath}" not found. Please check "componentPath" in "./ww-config.json". \x1b[0m`);
+        console.log(
+            `\x1b[41m "${componentPath}" not found. Please check "componentPath" in "./ww-config.json". \x1b[0m`
+        );
         return false;
     }
 
-    /*=============================================m_ÔÔ_m=============================================\
-        INDEX.JS
-    \================================================================================================*/
-    //Create index.js
-    const tempIndexJs = "./node_modules/@weweb/cli/assets/index.js";
-    if (fs.existsSync(tempIndexJs)) {
-        fs.unlinkSync(tempIndexJs);
+    if (fs.existsSync(TMP_INDEX_PATH)) {
+        fs.unlinkSync(TMP_INDEX_PATH);
     }
 
+
+    const RELATIVE_COMPONENT_PATH = path.join(RELATIVE_PATH, componentPath).split(path.sep).join(path.posix.sep);;
+    const RELATIVE_CONFIG_PATH = path.join(RELATIVE_PATH, `ww-config.${fileExtension}`).split(path.sep).join(path.posix.sep);
+
+    console.log({RELATIVE_COMPONENT_PATH, RELATIVE_CONFIG_PATH})
+
     let indexContent = `
-        import component from '../../../../${componentPath}'
-        import configuration from '../../../../ww-config.${fileExtension}'
+        import component from '${RELATIVE_COMPONENT_PATH}'
+        import configuration from '${RELATIVE_CONFIG_PATH}'
 
         const name = "__NAME__";
         const version = '__VERSION__';
@@ -88,7 +88,7 @@ exports.prebuild = (options = {}) => {
             }, 10);
         }`;
 
-    fs.writeFileSync(tempIndexJs, indexContent);
+    fs.writeFileSync(TMP_INDEX_PATH, indexContent);
 
     return true;
 };
